4794bd0eae034fad98fb5e1feecc7f67fc2c48b5
Saroj zoras <saroj@zoras.me>
2018-05-10 00:46:22 +0545

    Use netlify snippet injection https://www.netlify.com/docs/inject-analytics-snippets/

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/netlify_admin/";
-      });
-    }
-  });
-};
-
 document.addEventListener('DOMContentLoaded', function () {
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   if ($navbarBurgers.length > 0) {
