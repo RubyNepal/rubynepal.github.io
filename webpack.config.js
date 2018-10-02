cbc207bd65304251b6ea6c77d7b6670a6c32d6ea
Saroj zoras <sawrows@gmail.com>
2018-10-02 15:16:45 +0545

    Use single quote as per eslint rule

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,12 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
