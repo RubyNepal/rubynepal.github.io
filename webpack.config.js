e9e0485ef62f2911c9f75a5fdb9fcb77ea3d398a
Saroj Maharjan <zoras@users.noreply.github.com>
2022-09-13 14:25:29 +0200

    Npm audit fix (#732)
    * Ran npm audit fix --force
    
    * Add missing utilities after removal of default polyfills in webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,18 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      'stream': require.resolve('stream-browserify'),
+      'buffer': require.resolve('buffer/'),
+      'util': require.resolve('util/'),
+      'assert': require.resolve('assert/'),
+      'http': require.resolve('stream-http/'),
+      'url': require.resolve('url/'),
+      'https': require.resolve('https-browserify/'),
+      'os': require.resolve('os-browserify/'),
+    },
+   }
 };
